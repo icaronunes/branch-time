cf7040eff1621d5aacd0057f025f1c9d42ec7a97
Icaro <icarornunes@gmail.com>
2023-05-13 19:16:37 -0300

    create test - git not found

--- tests/test_branch.py
+++ tests/test_branch.py
@@ -3,7 +3,12 @@ from branch_time.jira import app
 runner = CliRunner()
 
 
-def test_result_lost_by_show_with_luck():
+def test_not_directory():
     result = runner.invoke(app, ["any_where", "--time", "1"])
     assert result.exit_code == 1
     assert "Directory not found" in result.output
+
+def test_not_directory_on_git():
+    result = runner.invoke(app, ["../../", "--time", "1"])
+    assert result.exit_code == 1
+    assert "repository parameter does not point to a git repository" in result.output
